5899650ae3c8388ea081470c5ac8d42a94a2c00e
Rohit Balage <thatcomputerboy26@gmail.com>
2025-01-22 12:49:16 -0500

    initial set up of app with typescript

--- App.tsx
+++ App.tsx
@@ -14,83 +14,23 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView >
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        contentInsetAdjustmentBehavior="automatic">
+        <Text style={styles.MainHeadling}>React Native Practice App</Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -113,6 +53,15 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  
+  MainHeadling: {
+    fontWeight: '600',       // Keeps the bold style
+    fontStyle: 'normal',     // Ensures the text is not italic
+    textAlign: 'center',     // Centers the text horizontally
+    fontSize: 24,            // Adjusts to a larger text size (adjust value as needed)
+    textDecorationLine: 'underline', // Adds an underline
+  }
+  
 });
 
 export default App;
